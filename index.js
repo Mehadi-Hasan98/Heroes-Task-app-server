2910061285314c40b33a9876f61c6f619ef51269
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-07-01 12:08:28 +0600

    Cors updated

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
+// enable cors
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+app.options(
+  '*',
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
