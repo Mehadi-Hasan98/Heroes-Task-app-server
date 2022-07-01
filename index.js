07d8e395cedd2bfedea496f127ede11bbe8dde88
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-07-01 11:30:44 +0600

    CORS policy updated

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const app = express();
 
 // middleware
 app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
 app.use(express.json());
 
 
